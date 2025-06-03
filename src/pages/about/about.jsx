72b1e92844a958079b3110888e22af2c61f95227
Dounia <hajjajidounia8@gmail.com>
2025-06-03 22:42:34 +0100

    about-header

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-           
+        <div className="min-h-screen bg-white">
+            {/* About Header */}
+            <div className="relative h-[300px] bg-black">
+                <img 
+                    src="/public/images/shopy4.webp" 
+                    alt="About Banner" 
+                    className="w-full h-full object-cover opacity-50"
+                />
+                <div className="absolute inset-0 flex items-center justify-center">
+                    <h1 className="text-5xl font-bold text-white">ABOUT</h1>
+                </div>
+            </div>
+
+            
         </div>
     );
 };
 
-export default About;
+export default About; 
