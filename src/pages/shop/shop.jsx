089401e2b1fe4b79749c2a3d4c4a169587eb4d99
Dounia <hajjajidounia8@gmail.com>
2025-06-05 10:55:59 +0100

    modif photo

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -141,7 +141,7 @@ const Shop = () => {
     return (
         <div className="min-h-screen bg-white">
             {/* Shop Header */}
-            <div className="relative h-[300px] bg-black">
+            <div className="relative h-[300px]">
                 <img 
                     src="/images/carousel2.jpg" 
                     alt="Shop Banner" 
@@ -261,7 +261,7 @@ const Shop = () => {
                                             alt={product.name}
                                             className="w-full h-[400px] object-cover transition-transform duration-500 group-hover:scale-110"
                                         />
-                                        <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-20 transition-all duration-300 flex items-center justify-center">
+                                        <div className="absolute inset-0 bg-opacity-0 group-hover:bg-opacity-20 transition-all duration-300 flex items-center justify-center">
                                             <button className="bg-white text-gray-900 px-6 py-2 rounded-full transform translate-y-full opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-300 hover:bg-gray-100">
                                                 ADD TO CART
                                             </button>
